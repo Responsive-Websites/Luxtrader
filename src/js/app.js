863958382c5bf59aa2cee90b69433c4612a8830c
Danila-hub <kontaigabox@mail.ru>
2022-05-30 19:05:09 +0300

    Added some header styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 //import * as flsFunctions from "./modules/functions.js";
 //flsFunctions.thisTest();
 
-
+let user_icon = document.querySelector('.user-header__icon');
+user_icon.addEventListener('click', function (e) {
+  let user_menu = document.querySelector('.user-header__menu');
+  user_menu.classList.toggle('_active');
+});
